ec4b6848bbc12e376feb63f19f644f450dee112c
Annette <annette@Annettes-MacBook-Air.local>
2020-09-15 17:52:46 -0600

    Week 1 Coding Assignment

---
+++ index.js
@@ -0,0 +1,39 @@
+var applePrice = .50;
+var laptopPrice = 200.00;
+var walletBalance1 = 45.00;
+var walletBalance2 = 325.00;
+var justinNumberOfFriends = 10;
+var samNumberOfFriends = 7;
+var ageInYears1 = 40;
+var ageInYears2 = 25;
+var firstName1 = "Jay ";
+var lastName1 = "Ryan";
+var middleInitial1 = "A ";
+var firstName2 = "Alice ";
+var lastName2 = "Ryan";
+var middleInitial2 = "B ";
+
+var newWalletBalance = walletBalance2 - laptopPrice;
+var totalNumberOfFriends = ageInYears1 * justinNumberOfFriends;
+var fullName = firstName1 + middleInitial1 + lastName1;
+
+
+console.log("Week1 Coding Assignment");
+console.log("The apple price is " + applePrice); 
+console.log("The laptop price is " + laptopPrice);
+console.log("The balance of wallet 1 is " + walletBalance1);
+console.log("The balance of wallet 2 is " + walletBalance2);
+console.log("The number of Justin's friends is " + justinNumberOfFriends);
+console.log("The number of Sam's friends is " + samNumberOfFriends);
+console.log("Justin's age is " + ageInYears1);
+console.log("Sam's age is " + ageInYears2);
+console.log("My friend's name is " + firstName1  + middleInitial1  + lastName1);
+console.log("My cousin's name is " + firstName2 + middleInitial2 + lastName2);
+
+console.log("The new wallet balance of wallet 2 is " + newWalletBalance);
+console.log("Justin's total number of friends is " + totalNumberOfFriends);
+console.log("My friend's name is " + fullName);
+
+
+
+
